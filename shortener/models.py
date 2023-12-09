f037d4e05047459b2174ea26affd3c89c2d3ab86
darrin <darrinb3@gmail.com>
2023-12-09 15:21:31 -0600

    models and admin page

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Shortener(models.Model):
+    DURATIONS = {
+        "1": "1",
+        "3": "3",
+        "6": "6",
+        "12": "12"
+    }
+    url = models.TextField(max_length=300)
+    short_key = models.TextField(max_length=30)
+    tags = models.TextField(max_length=100)
+    active_duration = models.CharField(max_length=2, choices=DURATIONS)
+    createdDate = models.DateTimeField(auto_now_add=True)
+
--- shortener/views.py
+++ shortener/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from shortener.models import Shortener
 
 
 class Index(View):
