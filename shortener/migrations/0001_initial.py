974f8bebbfe6cd2ca52a63176b9d223a7aca4376
dbrac <darrinb3@gmail.com>
2024-02-18 21:14:22 -0600

    increase URL field size

--- shortener/migrations/0001_initial.py
+++ shortener/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
             name='Shortener',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('url', models.TextField(max_length=300)),
+                ('url', models.TextField(max_length=1000)),
                 ('short_key', models.TextField(max_length=30)),
                 ('tags', models.TextField(max_length=100)),
                 ('active_duration', models.CharField(choices=[(1, 1), (3, 3), (6, 6), (12, 12)], max_length=2)),
